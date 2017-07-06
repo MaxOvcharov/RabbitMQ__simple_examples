ba869f693987d2fbccf328a27c56bdab53762ccb
MaxOvcharov <ovcharovmax@yandex.ru>
2017-07-06 22:59:02 +0300

    Added example_1 with simple producer sender

---
+++ exp1_simple_sender/send_producer.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import pika
+import time
+import sys
+
+from elizabeth import Text
+
+conn = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = conn.channel()
+channel.queue_declare(queue='client_msg_queue')
+
+
+def send_msg(payload):
+    channel.basic_publish(exchange='',
+                          routing_key='client_msg_queue',
+                          body=payload)
+
+
+def main():
+    try:
+        client_msg = Text()
+        payload = dict(message=None, msg_id=0)
+        msg_count = 1
+        while True:
+            payload['message'] = client_msg.sentence()
+            payload['msg_id'] = msg_count
+            send_msg(payload)
+            msg_count += 1
+            time.sleep(3)
+    except KeyboardInterrupt:
+        conn.close()
+        sys.exit()
+
+if __name__ == 'main':
+    main()
