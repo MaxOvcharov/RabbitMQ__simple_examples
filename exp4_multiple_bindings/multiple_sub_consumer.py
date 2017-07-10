929bb6b07728ba4656c3f8fa0ec386ccc786ed3e
MaxOvcharov <ovcharovmax@yandex.ru>
2017-07-10 23:33:19 +0300

    Bug fixed: set correct value for -w key

--- exp4_multiple_bindings/multiple_sub_consumer.py
+++ exp4_multiple_bindings/multiple_sub_consumer.py
@@ -5,6 +5,8 @@ import time
 
 from optparse import OptionParser
 
+SERVER_LIST = ['s_1', 's_2', 's_3']
+
 
 def parse_args_for_init_worker():
     """
@@ -16,32 +18,40 @@ def parse_args_for_init_worker():
                       help='ADDING DELAY INTO CALLBACK FUNCTION',
                       type='int', default=None)
     parser.add_option('-w', '--worker', dest='worker_number',
-                      help='MIGRATE TABLES',
-                      action='store_true', default=1)
+                      help='CREATE NUMBER OF WORKERS',
+                      type='int', default=1)
+    parser.add_option('-k', '--routing_key', dest='routing_key',
+                      help='CHOSE ROUTING KEY FROM SERVER_LIST = {}'.format(SERVER_LIST),
+                      type='int')
     options, args = parser.parse_args()
+    if not options.routing_key:
+        parser.error('Routing key is mandatory for running worker. Example: -routing_key 1,'
+                     ' choose the first routing_key from SERVER_LIST= {}'.format(SERVER_LIST))
 
     return options
 
 opt = parse_args_for_init_worker()
 callback_delay = opt.callback_delay
 worker_number = opt.worker_number
+routing_key = SERVER_LIST[opt.routing_key]
 task_counter = 1
 
 conn = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = conn.channel()
-channel.exchange_declare(exchange='logs', exchange_type='fanout')
+channel.exchange_declare(exchange='direct_message', exchange_type='direct')
+
 res = channel.queue_declare(exclusive=True)
 queue_name = res.method.queue
-channel.queue_bind(exchange='logs', queue=queue_name)
-print(' [*] Waiting for messages. Queue name: {0}, Delay: {1}, Number of workers: {2}.\n'
-      ' To exit press CTRL+C'.format(queue_name, callback_delay, worker_number))
+channel.queue_bind(exchange='direct_message', queue=queue_name, routing_key=routing_key)
+print(' [*] Waiting for messages. Routing key: {0}, Delay: {1}, Number of workers: {2}.\n'
+      ' To exit press CTRL+C'.format(routing_key, callback_delay, worker_number))
 
 
 def callback(ch, method, properties, body):
     global task_counter
     client_message = json.loads(body)
-    print(' [x] Received: %s, message_type: %s' % (client_message,
-                                                   type(client_message)))
+    print(' [x] Received: %s, message_type: %s, routing_key: %s'
+          % (client_message, type(client_message),method.routing_key))
     if callback_delay:
         time.sleep(callback_delay)
     ch.basic_ack(delivery_tag=method.delivery_tag)
