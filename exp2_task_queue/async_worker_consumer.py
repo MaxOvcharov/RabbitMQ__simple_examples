32c9dfbae5cc0bcbc3104696c58eb7740f1b4031
MaxOvcharov <ovcharovmax@yandex.ru>
2017-07-16 12:59:06 +0300

    Added new async version of worker_consumer

---
+++ exp2_task_queue/async_worker_consumer.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import json
+import pika
+import time
+
+from optparse import OptionParser
+
+
+def parse_args_for_init_worker():
+    """
+    Configurations of arg-parser for init worker
+    :return: options - a dict with input args
+    """
+    parser = OptionParser()
+    parser.add_option('-d', '--delay', dest='callback_delay',
+                      help='ADDING DELAY INTO CALLBACK FUNCTION',
+                      type='int', default=None)
+    parser.add_option('-w', '--worker', dest='worker_number',
+                      help='CREATE NUMBER OF WORKERS',
+                      type='int', default=1)
+    options, args = parser.parse_args()
+
+    return options
+
+opt = parse_args_for_init_worker()
+callback_delay = opt.callback_delay
+worker_number = opt.worker_number
+task_counter = 1
+
+conn = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = conn.channel()
+channel.queue_declare(queue='task_queue', durable=True)
+print(' [*] Waiting for messages. Delay:{0}, Number of workers: {1}.\n'
+      ' To exit press CTRL+C'.format(callback_delay, worker_number))
+
+
+def callback(ch, method, properties, body):
+    global task_counter
+    client_message = json.loads(body)
+    print(' [x] Received: {0}, message_type: {1}'.format(client_message,
+                                                         type(client_message)))
+    if callback_delay:
+        time.sleep(callback_delay)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    print('DONE TASK: {}'.format(task_counter))
+    task_counter += 1
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback, queue='task_queue')
+channel.start_consuming()
