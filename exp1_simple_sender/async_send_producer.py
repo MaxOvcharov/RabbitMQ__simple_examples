ce3e2249ba633bc8143449f57f3b7e3065f32fef
MaxOvcharov <ovcharovmax@yandex.ru>
2017-07-21 23:38:11 +0300

    Refactoring to python 3.5 style

--- exp1_simple_sender/async_send_producer.py
+++ exp1_simple_sender/async_send_producer.py
@@ -9,47 +9,44 @@ import json
 from elizabeth import Text
 
 
-@asyncio.coroutine
-def send_msg(payload, channel):
+async def send_msg(payload, channel):
     print(' [x] Send: {0}, message_type: {1}'.format(payload, type(payload)))
-    yield from channel.basic_publish(payload=payload, exchange_name='',
-                                     routing_key='client_msg_queue')
+    await channel.basic_publish(payload=payload, exchange_name='',
+                                routing_key='client_msg_queue')
 
 
-@asyncio.coroutine
-def worker():
+async def worker():
     try:
         client_msg = Text()
         payload = dict(message=None, msg_id=0, producer_type='ASYNC')
         msg_count = 1
 
-        transport, protocol = yield from aioamqp.connect()
-        channel = yield from protocol.channel()
-        yield from channel.queue_declare(queue_name='client_msg_queue')
+        transport, protocol = await aioamqp.connect()
+        channel = await protocol.channel()
+        await channel.queue_declare(queue_name='client_msg_queue')
         while True:
             payload['message'] = client_msg.sentence()
             payload['msg_id'] = msg_count
-            yield from send_msg(json.dumps(payload), channel)
+            await send_msg(json.dumps(payload), channel)
             msg_count += 1
-            yield from asyncio.sleep(0.003)
+            await asyncio.sleep(0.003)
     except aioamqp.AmqpClosedConnection:
         print("closed connections")
         return
     except KeyboardInterrupt:
-        yield from protocol.close()
-        transport.close()
+        await protocol.close()
+        await transport.close()
 
 
 def main():
-
     # Side note: Apparently, async() will be deprecated in 3.4.4.
     # See: https://docs.python.org/3.4/library/asyncio-task.html#asyncio.async
-    tasks = asyncio.gather(asyncio.async(worker()))
+    tasks = asyncio.gather(worker())
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(tasks)
         loop.run_forever()
-    except KeyboardInterrupt as e:
+    except KeyboardInterrupt:
         print("Caught keyboard interrupt. Canceling tasks...")
         tasks.cancel()
         loop.run_forever()
