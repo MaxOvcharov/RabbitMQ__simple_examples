033a142340d070495123b94d09dfb1ba3ef9e400
MaxOvcharov <ovcharovmax@yandex.ru>
2017-07-22 15:55:20 +0300

    Do some refuctoring. Added KeyboarInterrupt Error handler

--- exp1_simple_sender/receive_consumer.py
+++ exp1_simple_sender/receive_consumer.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-import pika
 import json
+import pika
+import sys
 
 conn = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = conn.channel()
 channel.queue_declare(queue='client_msg_queue')
-
-print(' [*] Waiting for messages. To exit press CTRL+C')
 
 
 def callback(ch, method, properties, body):
@@ -14,5 +13,15 @@ def callback(ch, method, properties, body):
     print(' [x] Received: {0}, message_type: {1}'.format(client_message,
                                                          type(client_message)))
 
-channel.basic_consume(callback, queue='client_msg_queue', no_ack=True)
-channel.start_consuming()
+
+def main():
+    try:
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        channel.basic_consume(callback, queue='client_msg_queue', no_ack=True)
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        conn.close()
+        sys.exit()
+
+if __name__ == '__main__':
+    main()
