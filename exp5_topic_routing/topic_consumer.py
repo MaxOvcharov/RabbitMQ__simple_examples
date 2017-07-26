4af51c04d2dc96827620ee2d203d7f330fe9c8ee
MaxOvcharov <ovcharovmax@yandex.ru>
2017-07-26 22:58:00 +0300

    Added new validation rule for routing key and handler for KeyboardInterrupt Error

--- exp5_topic_routing/topic_consumer.py
+++ exp5_topic_routing/topic_consumer.py
@@ -2,11 +2,12 @@
 import json
 import pika
 import time
+import sys
 
 from optparse import OptionParser
 
-SERVER_LIST = ['s1', 's2', 's3']
-MSG_TYPE = ['sendMessage', 'sendHistory', 'sendCallback']
+SERVER_LIST = ['s1', 's2', 's3', '*', '#']
+MSG_TYPE = ['sendMessage', 'sendHistory', 'sendCallback', '*', '#']
 
 
 def parse_args_for_init_worker():
@@ -18,13 +19,10 @@ def parse_args_for_init_worker():
     parser.add_option('-d', '--delay', dest='callback_delay',
                       help='ADDING DELAY INTO CALLBACK FUNCTION',
                       type='int', default=None)
-    parser.add_option('-w', '--worker', dest='worker_number',
-                      help='CREATE NUMBER OF WORKERS',
-                      type='int', default=1)
     parser.add_option('-k', '--routing_key', dest='routing_key',
                       help='CREATE ROUTING KEY WITH VALUE FROM SERVER_LIST = {0}'
                            ' AND VALUE FROM MSG_TYPE = {1}'.format(SERVER_LIST, MSG_TYPE),
-                      nargs=1, type='string')
+                      nargs=2, type='string')
     options, args = parser.parse_args()
     if not options.routing_key:
         parser.error('\nRouting key is mandatory for running worker.\n'
@@ -32,13 +30,15 @@ def parse_args_for_init_worker():
                      'You can use:\n1) * (star) can substitute for exactly one word; \n'
                      '2) # (hash) can substitute for zero or more words. \n'
                      'For more information use --help')
+    elif options.routing_key[0] not in SERVER_LIST or options.routing_key[1] not in MSG_TYPE:
+        parser.error('Some of the values in routing_key is wrong. '
+                     '\nSERVER: %s, MSG_TYPE: %s' % options.routing_key)
 
     return options
 
 opt = parse_args_for_init_worker()
 callback_delay = opt.callback_delay
-worker_number = opt.worker_number
-routing_key = opt.routing_key
+routing_key = '.'.join(opt.routing_key)
 task_counter = 1
 
 conn = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
@@ -47,8 +47,8 @@ channel.exchange_declare(exchange='topic_message', exchange_type='topic')
 res = channel.queue_declare(exclusive=True)
 queue_name = res.method.queue
 channel.queue_bind(exchange='topic_message', queue=queue_name, routing_key=routing_key)
-print(' [*] Waiting for messages. Routing key: {0}, Delay: {1}, Number of workers: {2}.\n'
-      ' To exit press CTRL+C'.format(routing_key, callback_delay, worker_number))
+print(' [*] Waiting for messages. Routing key: {0}, Delay: {1}.\n'
+      ' To exit press CTRL+C'.format(routing_key, callback_delay))
 
 
 def callback(ch, method, properties, body):
@@ -62,5 +62,14 @@ def callback(ch, method, properties, body):
     print('DONE TASK: {}'.format(task_counter))
     task_counter += 1
 
-channel.basic_consume(callback, queue=queue_name)
-channel.start_consuming()
+
+def main():
+    try:
+        channel.basic_consume(callback, queue=queue_name)
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        conn.close()
+        sys.exit()
+
+if __name__ == '__main__':
+    main()
